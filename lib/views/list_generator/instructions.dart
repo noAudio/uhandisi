a0b4793f79958527f44d0d3c31aa2b68c56aa827
Samuel Odongo <samieodd@gmail.com>
2022-02-21 18:53:45 +0300

    Refactor

--- lib/views/list_generator/instructions.dart
+++ lib/views/list_generator/instructions.dart
@@ -1,14 +1,12 @@
 import 'dart:html';
 
 class InstructionsView {
-  Element instructionsView(String title, String instructions) => DivElement()
-    ..className = 'instructions-section'
-    ..children = [
-      HeadingElement.h1()
-        ..text = title
-        ..className = 'instructions-title',
-      ParagraphElement()
-        ..text = instructions
-        ..className = 'instructions-text',
-    ];
+  List<Element> instructionsView(String title, String instructions) => [
+        HeadingElement.h1()
+          ..text = title
+          ..className = 'instructions-title',
+        ParagraphElement()
+          ..text = instructions
+          ..className = 'instructions-text',
+      ];
 }
