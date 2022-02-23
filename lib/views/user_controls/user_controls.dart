92bbea449a3661820d4ef286dd9788530f4991ec
Samuel Odongo <samieodd@gmail.com>
2022-02-23 22:57:15 +0300

    Added control elements

--- lib/views/user_controls/user_controls.dart
+++ lib/views/user_controls/user_controls.dart
@@ -4,6 +4,21 @@ class UserControls {
   DivElement userControls() {
     return DivElement()
       ..className = 'user-controls'
-      ..children = [];
+      ..children = [
+        DivElement()
+          ..className = 'save-section'
+          ..children.addAll([
+            HeadingElement.h4()..text = 'Save list',
+            InputElement()
+              ..className = 'save-textbox'
+              ..placeholder = 'Name',
+            ButtonElement()
+              ..className = 'save-button'
+              ..text = 'Save list',
+          ]),
+        ButtonElement()
+          ..className = 'reset-button'
+          ..text = 'RESET',
+      ];
   }
 }
