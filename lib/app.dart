c3f96509d8c9083199dbe823cc4fce1f628bba50
Samuel Odongo <samieodd@gmail.com>
2022-02-23 22:50:30 +0300

    Mounted UserControls component

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 import 'package:uhandisi/views/list_generator/list_generator.dart';
 import 'package:uhandisi/views/previous_list/previous_list.dart';
+import 'package:uhandisi/views/user_controls/user_controls.dart';
 
 class App {
   var root = querySelector('#root') as DivElement;
@@ -10,7 +11,11 @@ class App {
 
   void run() {
     root.children.addAll([
-      DivElement()..classes = ['section', 'left'],
+      DivElement()
+        ..classes = ['section', 'left']
+        ..append(
+          UserControls().userControls(),
+        ),
       DivElement()
         ..classes = ['section', 'mid']
         ..append(
@@ -18,7 +23,9 @@ class App {
         ),
       DivElement()
         ..classes = ['section', 'right']
-        ..append(PreviousListView().previousListView()),
+        ..append(
+          PreviousListView().previousListView(),
+        ),
     ]);
   }
 }
