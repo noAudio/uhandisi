65349b94898f30bad554dda29728d7cccc6074c5
Samuel Odongo <samieodd@gmail.com>
2022-02-23 21:22:30 +0300

    Updated file structure

--- lib/views/list_generator/list_generator.dart
+++ lib/views/list_generator/list_generator.dart
@@ -1,8 +1,8 @@
 import 'dart:html';
 
-import 'package:uhandisi/views/list_generator/instructions.dart';
-import 'package:uhandisi/views/list_generator/tab_switcher.dart';
-import 'package:uhandisi/views/list_generator/text_boxes.dart';
+import 'package:uhandisi/views/list_generator/components/instructions.dart';
+import 'package:uhandisi/views/list_generator/components/tab_switcher.dart';
+import 'package:uhandisi/views/list_generator/components/text_boxes.dart';
 
 class ListGeneratorView {
   final appInstructions = '''
