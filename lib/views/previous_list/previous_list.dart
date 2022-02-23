32038834379a7513eb3d2d15b2d6aaff69969259
Samuel Odongo <samieodd@gmail.com>
2022-02-23 22:34:58 +0300

    Refactored, added paragraph title

--- lib/views/previous_list/previous_list.dart
+++ lib/views/previous_list/previous_list.dart
@@ -2,11 +2,11 @@ import 'dart:html';
 
 /// This view shows a list of previous sessions that have been cached
 class PreviousListView {
-  DivElement previousListView([List<Element>? previousLists]) {
+  DivElement previousListView({List<Element>? previousLists}) {
     return DivElement()
       ..className = 'previous-list'
       ..children.addAll([
-        HeadingElement.h3()
+        HeadingElement.h4()
           ..text = 'Previous Lists'
           ..className = 'prev-title',
         DivElement()
@@ -16,6 +16,8 @@ class PreviousListView {
                 ParagraphElement()
                   ..className = 'lists-info'
                   ..text = 'No lists saved yet.'
+                  ..title =
+                      'You need to create and save lists for them to appear here.'
               ]),
       ]);
   }
