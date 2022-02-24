284ffc14c88329a0d09c9df5b6681a2e0528677f
Samuel Odongo <samieodd@gmail.com>
2022-02-25 00:45:46 +0300

    Linked minor category component to major category component

--- lib/views/sorted_materials/components/major_category.dart
+++ lib/views/sorted_materials/components/major_category.dart
@@ -1,7 +1,12 @@
 import 'dart:html';
 
+import 'package:uhandisi/views/sorted_materials/components/minor_category.dart';
+
 class MajorCategory {
-  DivElement majorCategory({required String id}) {
+  DivElement majorCategory({
+    required String id,
+    /*required Map<String, int> materials*/
+  }) {
     return DivElement()
       ..className = 'maj-category'
       ..id = id
@@ -9,6 +14,10 @@ class MajorCategory {
         HeadingElement.h3()
           ..className = 'maj-category-title'
           ..text = '${id.toUpperCase()} MATERIALS',
+        DivElement()
+          ..className = 'sas'
+          ..children.add(
+              MinorCategory().minorCategory(categoryName: 'MINOR CATEGORY')),
       ]);
   }
 }
