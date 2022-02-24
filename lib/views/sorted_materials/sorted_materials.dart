46825e9461f431c407d2a5b9b0753d392f620ada
Samuel Odongo <samieodd@gmail.com>
2022-02-24 21:56:48 +0300

    Connected MajorCategory component

--- lib/views/sorted_materials/sorted_materials.dart
+++ lib/views/sorted_materials/sorted_materials.dart
@@ -1,8 +1,26 @@
 import 'dart:html';
 
+import 'package:uhandisi/views/sorted_materials/components/major_category.dart';
+
 class SortedMaterials {
   bool isEmpty = true;
+
   DivElement sortedMaterials() {
-    return DivElement();
+    return DivElement()
+      ..className = 'sorted-section'
+      ..append(
+        //TODO: Change boolean to true
+        !isEmpty
+            ? (ParagraphElement()
+              ..className = 'sorted-info'
+              ..text = 'Please sort some materials above.')
+            : (DivElement()
+              ..className = 'categories'
+              ..children.addAll([
+                MajorCategory().majorCategory(id: 'manufactured'),
+                MajorCategory().majorCategory(id: 'raw'),
+                MajorCategory().majorCategory(id: 'encoded'),
+              ])),
+      );
   }
 }
