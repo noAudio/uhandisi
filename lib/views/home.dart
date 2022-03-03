a123ef75f4af1a5d25ed11ad85c5cacdf2aa1579
Samuel Odongo <samieodd@gmail.com>
2022-03-04 01:08:48 +0300

    Created file for home page widget

---
+++ lib/views/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:uhandisi/views/nav/nav_area.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          NavArea(),
+        ],
+      ),
+    );
+  }
+}
