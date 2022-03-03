9d58629bdf9f71450cba4c09254d07567b5b2627
Samuel Odongo <samieodd@gmail.com>
2022-03-04 01:08:25 +0300

    Moved pages and widgets to their own files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:uhandisi/views/home.dart';
 
 void main() {
   runApp(const App());
@@ -13,57 +13,6 @@ class App extends StatelessWidget {
       title: 'uhandisi - Painless Engineering',
       debugShowCheckedModeBanner: false,
       home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: const [
-          NavArea(),
-        ],
-      ),
-    );
-  }
-}
-
-class NavArea extends StatelessWidget {
-  const NavArea({
-    Key? key,
-  }) : super(key: key);
-
-  final LinearGradient linearGradient =
-      const LinearGradient(colors: [Color(0xFFFF6E7F), Color(0xFFBFE9FF)]);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          ShaderMask(
-            shaderCallback: ((Rect bounds) =>
-                linearGradient.createShader(Offset.zero & bounds.size)),
-            child: Text(
-              'uhandisi',
-              style: GoogleFonts.josefinSans(
-                textStyle: const TextStyle(
-                  fontSize: 59,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(),
-        ],
-      ),
     );
   }
 }
