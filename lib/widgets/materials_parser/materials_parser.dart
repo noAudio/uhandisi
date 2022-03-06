3487c133da366d519c64055def901eac1419d6ce
Samuel Odongo <samieodd@gmail.com>
2022-03-06 19:18:02 +0300

    Created textbox widget

--- lib/widgets/materials_parser/materials_parser.dart
+++ lib/widgets/materials_parser/materials_parser.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:uhandisi/widgets/materials_parser/components/tab_switcher.dart';
 import 'package:uhandisi/widgets/materials_parser/components/text_boxes.dart';
 
 class MaterialsParser extends StatelessWidget {
-  const MaterialsParser({Key? key}) : super(key: key);
+  const MaterialsParser({Key? key, required this.isCoriolis}) : super(key: key);
+
+  final bool isCoriolis;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
-        Flexible(
+      children: [
+        const Flexible(
           child: FractionallySizedBox(
             widthFactor: 0.2,
           ),
         ),
-        TextBoxes(
-          isCoriolis: false,
+        SizedBox(
+          width: 350,
+          child: Column(
+            children: [
+              TabSwitcher(isCoriolis: isCoriolis),
+              TextBoxes(isCoriolis: isCoriolis),
+            ],
+          ),
         ),
       ],
     );
