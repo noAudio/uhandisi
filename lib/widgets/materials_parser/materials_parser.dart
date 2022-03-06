e110f0d25a7c5cd35e2f0609bffcc6f19a6c44cd
Samuel Odongo <samieodd@gmail.com>
2022-03-07 00:05:46 +0300

    Created InstructionsPanel widget

--- lib/widgets/materials_parser/materials_parser.dart
+++ lib/widgets/materials_parser/materials_parser.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uhandisi/widgets/materials_parser/components/instructions_panel.dart';
 import 'package:uhandisi/widgets/materials_parser/components/submit_button.dart';
 import 'package:uhandisi/widgets/materials_parser/components/tab_switcher.dart';
 import 'package:uhandisi/widgets/materials_parser/components/text_boxes.dart';
@@ -7,15 +8,16 @@ class MaterialsParser extends StatelessWidget {
   const MaterialsParser({Key? key, required this.isCoriolis}) : super(key: key);
 
   final bool isCoriolis;
+  final String appInstructions =
+      '''Use the tab to switch between link mode and material list mode. In link mode, paste a coriolis link so that the app will generate a list of materials based on your build. In materials list mode, paste in the list of materials to have them sorted.''';
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Flexible(
-          child: FractionallySizedBox(
-            widthFactor: 0.2,
-          ),
+        const SizedBox(
+          width: 320,
         ),
         SizedBox(
           width: 350,
@@ -28,6 +30,9 @@ class MaterialsParser extends StatelessWidget {
               const SubmitButton(),
             ],
           ),
+        ),
+        InstructionsPanel(
+          instructionsText: appInstructions,
         ),
       ],
     );
