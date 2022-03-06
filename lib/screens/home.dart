93a3904cecc04939602e52f59dd924c3075a153c
Samuel Odongo <samieodd@gmail.com>
2022-03-06 19:19:46 +0300

    Added MaterialsParser widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uhandisi/widgets/materials_parser/materials_parser.dart';
 import 'package:uhandisi/widgets/nav/nav_area.dart';
 
 class Home extends StatelessWidget {
@@ -7,9 +8,13 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: const [
           NavArea(),
+          MaterialsParser(
+            isCoriolis: false,
+          ),
         ],
       ),
     );
