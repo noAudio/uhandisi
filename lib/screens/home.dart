4cbf35cea0fd1d70e220e444bf9987023c42397a
Samuel Odongo <samieodd@gmail.com>
2022-03-07 17:57:38 +0300

    Created layout for other sections

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uhandisi/widgets/materials_parser/materials_parser.dart';
 import 'package:uhandisi/widgets/nav/nav_area.dart';
+import 'package:uhandisi/widgets/previous_lists/previous_lists.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,13 +11,43 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
-        children: const [
-          NavArea(),
-          MaterialsParser(
+        children: [
+          const NavArea(),
+          const MaterialsParser(
             isCoriolis: false,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              SaveList(),
+              GeneratedLists(),
+              PreviousLists(),
+            ],
           ),
         ],
       ),
     );
+  }
+}
+
+class SaveList extends StatelessWidget {
+  const SaveList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class GeneratedLists extends StatelessWidget {
+  const GeneratedLists({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
