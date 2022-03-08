d981d4662de3ae72db41323cab5ae38292124342
Samuel Odongo <samieodd@gmail.com>
2022-03-08 22:21:18 +0300

    Fixed width and added major category components

--- lib/widgets/generated_lists/generated_lists.dart
+++ lib/widgets/generated_lists/generated_lists.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:uhandisi/widgets/generated_lists/components/major_category.dart';
 
 class GeneratedLists extends StatelessWidget {
   const GeneratedLists({
@@ -9,41 +9,70 @@ class GeneratedLists extends StatelessWidget {
   // TODO: Watch list of materials through state.
   final bool isMaterials = true;
 
+  final manList = const <Widget>[
+    Text('Galvanised Alloys'),
+    Text('Grid Resistors'),
+    Text('Focus Crystals'),
+    Text('Core Dynamic Composites'),
+    Text('Pharmaceutical Isolators'),
+    Text('Pharmaceutical Isolators'),
+    Text('Pharmaceutical Isolators'),
+    Text('Pharmaceutical Isolators'),
+    Text('Pharmaceutical Isolators'),
+    Text('Pharmaceutical Isolators'),
+    Text('Pharmaceutical Isolators'),
+    Text('Pharmaceutical Isolators'),
+  ];
+  final encList = const <Widget>[
+    Text('Modified Embedded Firmware'),
+    Text('Datamined Wake Exceptions'),
+    Text('Untypical Shield Scans'),
+    Text('Disrupted Wake Echoes'),
+    Text('Cracked Industrial Firmware'),
+    Text('Cracked Industrial Firmware'),
+    Text('Cracked Industrial Firmware'),
+    Text('Cracked Industrial Firmware'),
+    Text('Cracked Industrial Firmware'),
+    Text('Cracked Industrial Firmware'),
+    Text('Cracked Industrial Firmware'),
+    Text('Cracked Industrial Firmware'),
+  ];
+  final rawList = const <Widget>[
+    Text('Tin'),
+    Text('Selenium'),
+    Text('Vanadium'),
+    Text('Polonium'),
+    Text('Iron'),
+    Text('Iron'),
+    Text('Iron'),
+    Text('Iron'),
+    Text('Iron'),
+    Text('Iron'),
+    Text('Iron'),
+    Text('Iron'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      // width: 950,
-      height: 680,
+      width: 1150,
+      height: 700,
       child: Padding(
         padding: const EdgeInsets.fromLTRB(8.0, 30.0, 8.0, 8.0),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: !isMaterials
+          children: isMaterials
               ? [
-                  Column(
-                    children: [
-                      Text(
-                        'Manufactured Materials',
-                        style: GoogleFonts.poppins(
-                          fontSize: 24,
-                        ),
-                      ),
-                      // GridView.count(
-                      //   crossAxisCount: 2,
-                      //   mainAxisSpacing: 5,
-                      //   crossAxisSpacing: 5,
-                      //   children: const [
-                      //     Text('1'),
-                      //     Text('2'),
-                      //     Text('3'),
-                      //     Text('4'),
-                      //     Text('5'),
-                      //   ],
-                      // ),
-                    ],
-                  ),
-                  Column(),
-                  Column(),
+                  MajorCategory(
+                      majorCategoryTitle: 'Manufactured Materials',
+                      minorCategories: manList),
+                  MajorCategory(
+                      majorCategoryTitle: 'Encoded Materials',
+                      minorCategories: encList),
+                  MajorCategory(
+                      majorCategoryTitle: 'Raw Materials',
+                      minorCategories: rawList),
                 ]
               : [
                   const Text(
