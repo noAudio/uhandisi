4c9c6315a834053bac4a616f5a89c8b8ef98b6ff
Samuel Odongo <samieodd@gmail.com>
2022-03-08 23:16:33 +0300

    Created minor category widget

---
+++ lib/widgets/generated_lists/components/minor_category.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MinorCategory extends StatelessWidget {
+  const MinorCategory({
+    Key? key,
+    required this.minorCategoryTitle,
+    required this.processedMaterialItems,
+  }) : super(key: key);
+
+  final String minorCategoryTitle;
+  final List<ProcessedMaterialItem> processedMaterialItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            minorCategoryTitle,
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          ListView(
+            children: const [
+              ProcessedMaterialItem(materialText: 'materialText'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProcessedMaterialItem extends StatelessWidget {
+  const ProcessedMaterialItem({
+    Key? key,
+    required this.materialText,
+  }) : super(key: key);
+
+  final String materialText;
+  final bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(value: false, onChanged: (value) {}),
+        Text(materialText, style: GoogleFonts.poppins(fontSize: 16)),
+        isHovered
+            ? Row(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(Icons.remove),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(Icons.add),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(Icons.delete, color: Colors.red),
+                    ),
+                  ),
+                ],
+              )
+            : const SizedBox(),
+      ],
+    );
+  }
+}
