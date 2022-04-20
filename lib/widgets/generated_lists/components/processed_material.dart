807e5dbde0e24983a8e639f352f2a632ac275961
Samuel Odongo <samieodd@gmail.com>
2022-04-21 00:22:21 +0300

    Removed hover controls
    Hover controls as a feature will be re evaluated later

--- lib/widgets/generated_lists/components/processed_material.dart
+++ lib/widgets/generated_lists/components/processed_material.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:uhandisi/widgets/generated_lists/components/hover_controls.dart';
+// import 'package:uhandisi/widgets/generated_lists/components/hover_controls.dart';
 
 class ProcessedMaterialItem extends StatefulWidget {
   const ProcessedMaterialItem({
@@ -19,26 +19,19 @@ class _ProcessedMaterialItemState extends State<ProcessedMaterialItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: MouseRegion(
-        onEnter: (event) {
-          setState(() {
-            isHovered = true;
-          });
-        },
-        onExit: (event) {
-          setState(() {
-            isHovered = false;
-          });
-        },
-        child: Row(
-          children: [
-            Checkbox(value: false, onChanged: (value) {}),
-            Text(widget.materialText, style: GoogleFonts.poppins(fontSize: 16)),
-            isHovered ? const MaterialItemHoverControls() : const SizedBox(),
-          ],
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Checkbox(value: false, onChanged: (value) {}),
+        Expanded(
+          child: Text(
+            widget.materialText,
+            style: GoogleFonts.poppins(fontSize: 16),
+            overflow: TextOverflow.visible,
+          ),
         ),
-      ),
+        // isHovered ? const MaterialItemHoverControls() : const SizedBox(),
+      ],
     );
   }
 }
