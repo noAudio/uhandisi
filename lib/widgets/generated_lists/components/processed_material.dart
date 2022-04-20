b12fbbf1d51e41508cbb49dd3ff875d6345fd219
Samuel Odongo <samieodd@gmail.com>
2022-04-21 01:33:14 +0300

    Tracked checkbox state

--- lib/widgets/generated_lists/components/processed_material.dart
+++ lib/widgets/generated_lists/components/processed_material.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:uhandisi/widgets/generated_lists/components/hover_controls.dart';
 
 class ProcessedMaterialItem extends StatefulWidget {
   const ProcessedMaterialItem({
@@ -16,21 +15,37 @@ class ProcessedMaterialItem extends StatefulWidget {
 
 class _ProcessedMaterialItemState extends State<ProcessedMaterialItem> {
   bool isHovered = false;
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Checkbox(value: false, onChanged: (value) {}),
-        Expanded(
-          child: Text(
-            widget.materialText,
-            style: GoogleFonts.poppins(fontSize: 16),
-            overflow: TextOverflow.visible,
+        Checkbox(
+          value: isChecked,
+          visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
+          onChanged: (value) {
+            setState(() {
+              isChecked = !isChecked;
+            });
+          },
+        ),
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 4),
+            child: Text(
+              widget.materialText,
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                decoration: isChecked
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+                color: isChecked ? Colors.grey : Colors.black,
+              ),
+            ),
           ),
         ),
-        // isHovered ? const MaterialItemHoverControls() : const SizedBox(),
       ],
     );
   }
