96a4837bfb5e5b939e3cb4e902af14601d21df73
Samuel Odongo <samieodd@gmail.com>
2022-04-21 10:33:17 +0300

    Adjusted padding

--- lib/widgets/generated_lists/components/minor_category.dart
+++ lib/widgets/generated_lists/components/minor_category.dart
@@ -15,7 +15,7 @@ class MinorCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(2.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
