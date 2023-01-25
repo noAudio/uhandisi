4bb0f1f4a61f5418ece864a5b229b431eac98c2a
Samuel Odongo <samieodd@gmail.com>
2023-01-26 02:30:56 +0300

    Create actions
    Actions created to handle switching input type and getting user input

---
+++ lib/actions/get_user_input_action.dart
@@ -0,0 +1,5 @@
+class GetUserInputAction {
+  final String input;
+
+  GetUserInputAction({required this.input});
+}
--- lib/actions/index.dart
+++ lib/actions/index.dart
@@ -2,3 +2,5 @@ export 'add_coriolis_link_action.dart';
 export 'add_user_materials_action.dart';
 export 'complete_material_action.dart';
 export 'set_ship_and_build_name_action.dart';
+export 'switch_selection_action.dart';
+export 'get_user_input_action.dart';
