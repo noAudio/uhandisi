d6f3c23eb0b87b83bc31de549c564cc00893e357
Samuel Odongo <samieodd@gmail.com>
2023-01-26 02:33:25 +0300

    Handle actions being passed in
    Add input selection and user input to state

--- lib/reducers/app_reducer.dart
+++ lib/reducers/app_reducer.dart
@@ -1,7 +1,27 @@
+import 'package:uhandisi/actions/index.dart';
 import 'package:uhandisi/models/app_state.dart';
 
 AppState appReducer(AppState state, dynamic action) {
-  // TODO: Create switch case for available actions.
+  if (action is SwitchSelectionAction) {
+    return AppState(
+        shipName: state.shipName,
+        buildName: state.buildName,
+        coriolisLink: state.coriolisLink,
+        materials: state.materials,
+        userInput: state.userInput,
+        completedMaterials: state.completedMaterials,
+        selectedInput: action.selectedInput);
+  } else if (action is GetUserInputAction) {
+    return AppState(
+      shipName: state.shipName,
+      buildName: state.buildName,
+      coriolisLink: state.coriolisLink,
+      materials: state.materials,
+      userInput: action.input,
+      completedMaterials: state.completedMaterials,
+      selectedInput: state.selectedInput,
+    );
+  }
 
   return state;
 }
