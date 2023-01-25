b24cc22394c71a750436e8427b7817822699abd1
Samuel Odongo <samieodd@gmail.com>
2023-01-25 23:06:51 +0300

    Fix import error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:uhandisi/main.dart';
+import 'package:uhandisi/screens/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
