af6ce8a38e8b7d41738bcd16a5dcc5dc0ffc7c4d
Samuel Odongo <samieodd@gmail.com>
2023-01-25 23:27:36 +0300

    Create enum for user input type selection

---
+++ lib/enums/selected_input.dart
@@ -0,0 +1,6 @@
+/// This enum determines the type of input the user
+/// wants to use to generate their data.
+/// To generate from a coriolis link, use `coriolisLink`.
+/// To generate from a ready list of materials,
+/// use `materialList`.
+enum SelectedInput { coriolisLink, materialList }
