90010bccdf77902bd0fbf6dc11ca69744ea1f4b1
Samuel Odongo <samieodd@gmail.com>
2023-01-26 02:34:12 +0300

    Abstract common text styles

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles {
+  static TextStyle selectionButtonUnselected = GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.normal,
+      color: Colors.grey,
+    ),
+  );
+
+  static TextStyle selectionButtonSelected = GoogleFonts.poppins(
+    textStyle: const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+  );
+}
