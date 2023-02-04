9260f6ec052c2fe9e6c900df5ac1b2f0c13d289d
Samuel Odongo <samieodd@gmail.com>
2023-02-05 01:05:55 +0300

    Restrict aboutdialog size

--- lib/widgets/nav/components/settings_buttons.dart
+++ lib/widgets/nav/components/settings_buttons.dart
@@ -12,21 +12,30 @@ class SettingsButtons extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: [
         IconButton(
-          icon: const Icon(Icons.info_outline),
+          icon: const Icon(
+            Icons.info_outline,
+            color: Color(0xFFBFE9FF),
+          ),
           tooltip: 'Instructions on how to use this app.',
           splashRadius: 18.0,
           iconSize: 18.0,
           onPressed: () => aboutApp(context),
         ),
         IconButton(
-          icon: const Icon(Icons.restart_alt_outlined),
+          icon: const Icon(
+            Icons.restart_alt_outlined,
+            color: Color(0xFFBFE9FF),
+          ),
           tooltip: 'Reset materials.',
           splashRadius: 18.0,
           iconSize: 18.0,
           onPressed: () {},
         ),
         IconButton(
-          icon: const Icon(Icons.light_mode_outlined),
+          icon: const Icon(
+            Icons.light_mode_outlined,
+            color: Color(0xFFBFE9FF),
+          ),
           tooltip: 'Current: Light Mode',
           splashRadius: 18.0,
           iconSize: 18.0,
@@ -39,6 +48,7 @@ class SettingsButtons extends StatelessWidget {
 
 void aboutApp(BuildContext context) {
   return showAboutDialog(
+    applicationLegalese: '❤️ Built by Samuel Odongo',
     context: context,
     applicationIcon: const Logo(
       isMobile: true,
@@ -48,17 +58,36 @@ void aboutApp(BuildContext context) {
     applicationName: 'Painless Engineering',
     applicationVersion: '0.0.1',
     children: [
-      const Text(
-        'Instructions:',
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
+      SizedBox(
+        width: 250,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Instructions:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+                'Use the "Coriolis link" tab to generate and sort materials from a coriolis link. Please use the shortlink from the coriolis site.'),
+            Divider(),
+            Text(
+                'Use the "Materials list" tab to sort materials. The materials should be in the format "Material: Amount" e.g. "Iron: 5" and should each be in their own line.'),
+          ],
         ),
       ),
-      const Text(
-          'Use the "Coriolis link" tab to generate and sort materials from a coriolis link. Please use the shortlink from the coriolis site.'),
-      const Divider(),
-      const Text(
-          'Use the "Materials list" tab to sort materials. The materials should be in the format "Material: Amount" e.g. "Iron: 5" and should each be in their own line.'),
+      // const Text(
+      //   'Instructions:',
+      //   style: TextStyle(
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      // ),
+      // const Text(
+      //     'Use the "Coriolis link" tab to generate and sort materials from a coriolis link. Please use the shortlink from the coriolis site.'),
+      // const Divider(),
+      // const Text(
+      //     'Use the "Materials list" tab to sort materials. The materials should be in the format "Material: Amount" e.g. "Iron: 5" and should each be in their own line.'),
     ],
   );
 }
