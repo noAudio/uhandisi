028b766d1afd477be91a087f01b8249ff86baca0
Samuel Odongo <samieodd@gmail.com>
2023-02-08 03:20:03 +0300

    Add actions for conversion and sorting of material

--- lib/actions/index.dart
+++ lib/actions/index.dart
@@ -6,3 +6,5 @@ export 'switch_selection_action.dart';
 export 'get_user_input_action.dart';
 export 'validation_input_error_action.dart';
 export 'material_computation_action.dart';
+export 'convert_user_input_action.dart';
+export 'sort_materials_action.dart';
--- lib/reducers/app_reducer.dart
+++ lib/reducers/app_reducer.dart
@@ -1,5 +1,6 @@
 import 'package:uhandisi/actions/index.dart';
 import 'package:uhandisi/models/app_state.dart';
+import 'package:uhandisi/models/material_item.dart';
 
 AppState appReducer(AppState state, dynamic action) {
   if (action is SwitchSelectionAction) {
@@ -69,6 +70,31 @@ AppState appReducer(AppState state, dynamic action) {
         isComputing: action.isComputing,
         completedMaterials: state.completedMaterials,
         selectedInput: state.selectedInput);
+  } else if (action is ConvertUserInputAction) {
+    List<String> userInputs = state.userInput.split('/n');
+    List<MaterialItem> userMaterials = [];
+
+    for (String item in userInputs) {
+      if (item.contains(':')) {
+        var splitItem = item.split(':');
+        userMaterials.add(MaterialItem(
+          name: splitItem[0],
+          amount: int.parse(splitItem[1]),
+        ));
+      }
+    }
+
+    return AppState(
+      shipName: state.shipName,
+      buildName: state.buildName,
+      coriolisLink: state.coriolisLink,
+      materials: userMaterials,
+      userInput: state.userInput,
+      validationError: state.validationError,
+      isComputing: state.isComputing,
+      completedMaterials: state.completedMaterials,
+      selectedInput: state.selectedInput,
+    );
   }
 
   return state;
