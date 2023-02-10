6b70fdee5b7b544d3ab43d5cee0d3c2844f063e2
Samuel Odongo <samieodd@gmail.com>
2023-02-10 17:11:31 +0300

    Make checkboxes stateful to track completed items

--- lib/widgets/generated_materials/components/material_data.dart
+++ lib/widgets/generated_materials/components/material_data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uhandisi/models/material_item.dart';
 
-class MaterialData extends StatelessWidget {
+class MaterialData extends StatefulWidget {
   const MaterialData({
     Key? key,
     required this.material,
@@ -10,15 +10,39 @@ class MaterialData extends StatelessWidget {
   final MaterialItem material;
 
   @override
+  State<MaterialData> createState() => _MaterialDataState();
+}
+
+class _MaterialDataState extends State<MaterialData> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Checkbox(
-          value: false,
-          onChanged: (value) {},
+          value: isChecked,
+          onChanged: (value) {
+            setState(() {
+              isChecked = !isChecked;
+            });
+          },
         ),
-        Text('${material.name}: '),
-        Text('${material.amount}')
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              isChecked = !isChecked;
+            });
+          },
+          child: Text(
+            '${widget.material.name}: ${widget.material.amount}',
+            style: TextStyle(
+              decoration:
+                  isChecked ? TextDecoration.lineThrough : TextDecoration.none,
+              fontStyle: isChecked ? FontStyle.italic : FontStyle.normal,
+            ),
+          ),
+        ),
       ],
     );
   }
