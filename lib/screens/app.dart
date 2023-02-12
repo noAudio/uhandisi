1cc2655483ce0148df033ffd8f6f20edcde6089e
Samuel Odongo <samieodd@gmail.com>
2023-02-12 08:26:39 +0300

    Change settings buttons into FABs

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:uhandisi/actions/index.dart';
 import 'package:uhandisi/models/app_state.dart';
+import 'package:uhandisi/widgets/common/about_app.dart';
 import 'package:uhandisi/widgets/common/logo.dart';
-import 'package:uhandisi/widgets/nav/nav_area.dart';
 import 'package:uhandisi/widgets/top_layout.dart';
 
 class App extends StatelessWidget {
@@ -19,6 +20,7 @@ class App extends StatelessWidget {
           ),
           darkTheme: ThemeData.dark(),
           themeMode: state.themeMode,
+          // themeMode: ThemeMode.light,
           title: 'uhandisi - Painless Engineering',
           debugShowCheckedModeBanner: false,
           home: Scaffold(
@@ -30,6 +32,47 @@ class App extends StatelessWidget {
               centerTitle: true,
             ),
             body: const TopLayout(),
+            floatingActionButton: Wrap(
+              direction: Axis.horizontal,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(3),
+                  child: FloatingActionButton.extended(
+                    // TODO: Change from an aboutDialog to modal.
+                    onPressed: () => aboutApp(context),
+                    label: const Text('Instructions'),
+                    icon: const Icon(Icons.info_outline),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(3),
+                  child: FloatingActionButton.extended(
+                    onPressed: () {
+                      StoreProvider.of<AppState>(context)
+                          .dispatch(ResetMaterialsAction());
+                    },
+                    label: const Text('Reset'),
+                    icon: const Icon(Icons.restart_alt_outlined),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(3),
+                  child: FloatingActionButton.extended(
+                    onPressed: () {
+                      StoreProvider.of<AppState>(context)
+                          .dispatch(ChangeThemeAction());
+                    },
+                    label: Text(
+                        'Theme: ${state.themeMode == ThemeMode.light ? "Light" : "Dark"}'),
+                    icon: Icon(state.themeMode == ThemeMode.light
+                        ? Icons.light_mode_outlined
+                        : Icons.dark_mode_outlined),
+                  ),
+                ),
+              ],
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.miniCenterFloat,
           ),
         );
       },
